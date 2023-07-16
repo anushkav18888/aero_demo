c4fb81d64a3be6f1702ad35ca72665ea1fecc865
anushkav18888 <139616140+anushkav18888@users.noreply.github.com>
2023-07-17 03:00:38 +0530

    prevented next and previous overlap of 760 px width screen

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -10,14 +10,14 @@ function SampleNextArrow(props: any) {
   const { onClick } = props;
   return (
     <div
-      className="w-44 h-8 absolute bottom-32 z-30 right-10 border-[1px] border-gray-900 px-2 hover:border-gray-800 bg-black/50 hover:bg-black shadow-btnShadow overflow-hidden"
+      className="w-36 h-8 md:w-48 md:h-10 absolute bottom-10 md:bottom-32 z-30 right-4 md:right-10 border-[1px] border-gray-900 px-2 hover:border-gray-800 bg-black/50 hover:bg-black shadow-btnShadow overflow-hidden"
       onClick={onClick}
     >
-      <div className="w-full h-full text-gray-300 text-sm uppercase relative flex items-center justify-end cursor-pointer group  ">
-        <span className="absolute -translate-x-28 translate-y-0 group-hover:-translate-y-7 transition-transform duration-500">
+      <div className="w-full h-full text-gray-300 text-sm md:text-base uppercase relative flex items-end justify-end cursor-pointer group">
+        <span className="absolute -translate-x-14 md:-translate-x-28 translate-y-0 group-hover:-translate-y-7 transition-transform duration-500">
           next
         </span>
-        <span className="absolute -translate-x-28 translate-y-7 group-hover:translate-y-0 transition-transform duration-500">
+        <span className="absolute -translate-x-14 md:-translate-x-28 translate-y-7 md:translate-y-0 transition-transform duration-500">
           next
         </span>
         <span className="text-lg">
@@ -32,18 +32,18 @@ function SamplePrevArrow(props: any) {
   const { onClick } = props;
   return (
     <div
-      className="w-44 h-8 absolute bottom-32 z-30 left-10 border-[1px] border-gray-900 px-2 hover:border-gray-800 bg-black/50 hover:bg-black shadow-btnShadow overflow-hidden"
+      className="w-36 h-8 md:w-48 md:h-10 absolute bottom-10 md:bottom-32 z-30 left-4 md:left-10 border-[1px] border-gray-900 px-2 hover:border-gray-800 bg-black/50 hover:bg-black shadow-btnShadow overflow-hidden"
       onClick={onClick}
-      style={{backgroundColor:"#18181B"}}
+      style={{ backgroundColor: "#18181B" }}
     >
-      <div className="w-full h-full text-gray-300 text-sm uppercase relative flex items-center justify-between cursor-pointer group  ">
+      <div className="w-full h-full text-gray-300 text-sm md:text-base uppercase relative flex items-end justify-between cursor-pointer group">
         <span className="text-lg">
           <FaChevronLeft />
         </span>
-        <span className="absolute translate-x-24 translate-y-0 group-hover:-translate-y-7 transition-transform duration-500">
+        <span className="absolute translate-x-14 md:translate-x-24 translate-y-0 group-hover:-translate-y-7 transition-transform duration-500">
           previous
         </span>
-        <span className="absolute translate-x-24 translate-y-7 group-hover:translate-y-0 transition-transform duration-500">
+        <span className="absolute translate-x-14 md:translate-x-24 translate-y-7 md:translate-y-0 transition-transform duration-500">
           previous
         </span>
       </div>
@@ -64,7 +64,7 @@ const Banner = () => {
   return (
     <div className="w-full h-auto md:h-[650px] relative">
       <Slider {...settings}>
-        <div>
+        <div className="relative">
           <Image
             className="w-full h-auto md:h-[650px] object-cover"
             src={bannerImgOne}
@@ -72,7 +72,7 @@ const Banner = () => {
             alt="bannerImgOne"
           />
         </div>
-        <div>
+        <div className="relative">
           <Image
             className="w-full h-auto md:h-[650px] object-cover"
             src={bannerImgTwo}
@@ -80,7 +80,7 @@ const Banner = () => {
             alt="bannerImgTwo"
           />
         </div>
-        <div>
+        <div className="relative">
           <Image
             className="w-full h-auto md:h-[650px] object-cover"
             src={bannerImgThree}
@@ -88,7 +88,7 @@ const Banner = () => {
             alt="bannerImgThree"
           />
         </div>
-        <div>
+        <div className="relative">
           <Image
             className="w-full h-auto md:h-[650px] object-cover"
             src={bannerImgFour}
@@ -97,6 +97,8 @@ const Banner = () => {
           />
         </div>
       </Slider>
+      <div className="mt-8 md:mt-12" /> {/* Add margin to create space */}
+      <p>Following text...</p> {/* Add text with appropriate margin */}
     </div>
   );
 };
