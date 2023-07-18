1c1f36150299074f726a6392d954decbba015f24
anushkav18888 <139616140+anushkav18888@users.noreply.github.com>
2023-07-18 15:16:46 +0530

    added facebook again

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 // import FacebookProvider from "next-auth/providers/facebook";
-import InstagramProvider from "next-auth/providers/instagram";
+import FacebookProvider from "next-auth/providers/facebook";
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email";
 // import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -26,9 +26,13 @@ export const authOptions = {
   //   clientId: process.env.FACEBOOK_CLIENT_ID,
   //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET
   // })
-    InstagramProvider({
-    clientId: process.env.INSTAGRAM_CLIENT_ID,
-    clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
+  //   InstagramProvider({
+  //   clientId: process.env.INSTAGRAM_CLIENT_ID,
+  //   clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
+  // })
+    FacebookProvider({
+    clientId: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET
   })
     // EmailProvider({
     //   server: {
