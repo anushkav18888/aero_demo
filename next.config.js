dfbb2d9e691c03d066e361da10b310e2eb29e01b
anushkav1888 <anushkav1888@gmail.com>
2024-03-06 23:55:50 +0530

    added updated code

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  /**assetPrefix: '/aea', */
+  
   images: {
     domains: ["cdn.sanity.io"],
     path: "/aea/_next"
